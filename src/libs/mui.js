2bc5ed144d1a11903c27d6821f80324fc0d4ee8d
Mateus Barragana <mateus.barragana@gmail.com>
2021-02-01 17:05:58 -0300

    Adds mui exports file

---
+++ src/libs/mui.js
@@ -0,0 +1,30 @@
+// Material UI Components
+export { default as AppBar } from '@material-ui/core/AppBar';
+export { default as Card } from '@material-ui/core/Card';
+export { default as CardActionArea } from '@material-ui/core/CardActionArea';
+export { default as CardContent } from '@material-ui/core/CardContent';
+export { default as Grid } from '@material-ui/core/Grid';
+export { default as Typography } from '@material-ui/core/Typography';
+export { default as Toolbar } from '@material-ui/core/Toolbar';
+export { default as IconButton } from '@material-ui/core/IconButton';
+export { default as Fab } from '@material-ui/core/Fab';
+export { default as LinearProgress } from '@material-ui/core/LinearProgress';
+export { default as Tabs } from "@material-ui/core/Tabs";
+export { default as Tab } from "@material-ui/core/Tab";
+export { default as ExpansionPanel } from "@material-ui/core/ExpansionPanel";
+export { default as ExpansionPanelSummary } from "@material-ui/core/ExpansionPanelSummary";
+export { default as ExpansionPanelDetails } from "@material-ui/core/ExpansionPanelDetails";
+export { default as ExpandMoreIcon } from "@material-ui/icons/ExpandMore";
+export { default as Button } from '@material-ui/core/Button';
+export { default as Input } from '@material-ui/core/Input';
+export { default as InputLabel } from '@material-ui/core/InputLabel';
+export { default as FormControl } from '@material-ui/core/FormControl';
+export { default as Select } from '@material-ui/core/Select';
+export { default as MenuItem } from '@material-ui/core/MenuItem';
+export { default as Divider } from '@material-ui/core/Divider';
+
+// Material UI Icons
+export { default as AddIcon } from '@material-ui/icons/Add';
+export { default as EditIcon } from '@material-ui/icons/Edit';
+export { default as ArrowBackIcon } from "@material-ui/icons/ArrowBack";
+
