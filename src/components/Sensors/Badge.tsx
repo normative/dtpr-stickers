df46d41642447a543c9e0301fc24293a3847e279
Mateus Barragana <mateus.barragana@gmail.com>
2021-02-11 23:08:22 -0300

    Refactores sensor page completely

---
+++ src/components/Sensors/Badge.tsx
@@ -0,0 +1,45 @@
+import { Option } from 'common/types';
+import React from 'react';
+import { createStyles, withStyles, Theme } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
+
+interface Props {
+  option?: Option;
+  classes: any;
+}
+
+function Badge({ option, classes }: Props) {
+  if (!option) return <></>;
+
+  return (
+    <div className={classes.root}>
+      <img
+        className={classes.image}
+        src={`/images/${option.iconShortname}.svg`}
+        alt="purpose badge icon"
+      />
+      <Typography variant="subtitle2">
+        {option.name}
+      </Typography>
+    </div>
+  );
+}
+
+Badge.defaultProps = {
+  option: undefined,
+};
+
+const styles = (theme: Theme) => createStyles({
+  root: {
+    flex: 1,
+    textAlign: 'center',
+  },
+  image: {
+    height: '48px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginBottom: theme.spacing(),
+  },
+});
+
+export default withStyles(styles)(Badge);
