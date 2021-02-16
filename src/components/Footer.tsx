abef41e87643d6d5c6d51a5feef06bea36c9e501
Mateus Barragana <mateus.barragana@gmail.com>
2021-02-15 22:56:21 -0300

    Amends Footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,16 +4,20 @@ import {
   createStyles, Theme, WithStyles, withStyles,
 } from '@material-ui/core/styles';
 
-import { Button, Typography } from 'libs/mui';
+import { Typography } from 'libs/mui';
 
 function Footer({ classes }: WithStyles) {
   return (
     <div className={classes.root}>
-      <Typography align="center">
+      <Typography className={classes.text}>
         This is an open-source prototype from the Digital Transparency in the Public Realm project.
+        {' '}
+        We may share feedback on this page with Helpful Places.
       </Typography>
-      <Link to="/" style={{ textDecoration: 'none', width: '100%', textAlign: 'center' }}>
-        <Button style={{ backgroundColor: 'transparent', width: '100%' }} color="secondary">Learn more here</Button>
+      <Link to="/">
+        <Typography className={classes.link} color="primary">
+          LEARN MORE
+        </Typography>
       </Link>
     </div>
   );
@@ -30,6 +34,20 @@ const styles = (theme: Theme) => createStyles({
     '@media print': {
       display: 'none',
     },
+  },
+  text: {
+    color: '#515151',
+    fontSize: '0.75rem',
+    letterSpacing: '0.75px',
+    textAlign: 'center',
+  },
+  link: {
+    fontSize: '0.75rem',
+    fontWeight: 'bold',
+    letterSpacing: '0.25px',
+    marginTop: theme.spacing(1.25),
+    textAlign: 'center',
+    textDecorationLine: 'underline',
   },
 });
 
